77712b163a93ddcfd3469ef9642ec5bbaa32a02a
AaravSingh080 <Aaravsingh200816@icloud.com>
2026-04-17 03:36:58 +0530

    Delete knowledgeLoader.js

--- knowledgeLoader.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export function loadKnowledge(filePath) {
-  const resolvedPath = path.resolve(process.cwd(), filePath);
-  return fs.readFileSync(resolvedPath, "utf8");
-}
